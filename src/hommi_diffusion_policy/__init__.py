6251c06b4182f35ea1ede6523fb038a6d0651681
umeow0716 <jj30462281@gmail.com>
2026-08-10 18:42:43 +0800

    fix

--- src/hommi_diffusion_policy/__init__.py
+++ src/hommi_diffusion_policy/__init__.py
@@ -1,22 +1,16 @@
 from .common.normalizer import LinearNormalizer, SingleFieldLinearNormalizer
 from .policy.base import BasePolicy
-from .policy.dit import DiffusionDiTImagePolicy, DiffusionDiTPolicy
-from .policy.transformer import (
-    DiffusionTransformerPolicy,
-    DiffusionTransformerTimmPolicy,
-)
-from .policy.unet import DiffusionUnetPolicy, DiffusionUnetTimmPolicy
+from .policy.dit import DiffusionDiTImagePolicy
+from .policy.transformer import DiffusionTransformerPolicy
+from .policy.unet import DiffusionUnetPolicy
 
 __all__ = [
     "BasePolicy",
     "LinearNormalizer",
     "SingleFieldLinearNormalizer",
     "DiffusionUnetPolicy",
-    "DiffusionUnetTimmPolicy",
     "DiffusionTransformerPolicy",
-    "DiffusionTransformerTimmPolicy",
     "DiffusionDiTImagePolicy",
-    "DiffusionDiTPolicy",
 ]
 
 __version__ = "0.1.3"
